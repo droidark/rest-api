b7092be7c28178c8fa8eaa49df16289e77f8a993
droidark <droidark@outlook.com>
2020-12-16 18:05:07 -0600

    Add configuration in main class.

--- src/main/java/xyz/krakenkat/restapi/RestApiApplication.java
+++ src/main/java/xyz/krakenkat/restapi/RestApiApplication.java
@@ -1,8 +1,27 @@
 package xyz.krakenkat.restapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "REST API",
+                version = "1.0",
+                description = "Tutorial about REST API with Spring Boot",
+                license = @License(name = "Creative Commons Attribution 4.0 International License.",
+                        url = "https://creativecommons.org"),
+                contact = @Contact(name = "Fozz", email = "droidark@outlook.com", url = "https://github.com/droidark")
+        )
+)
 @SpringBootApplication
 public class RestApiApplication {
 
@@ -10,4 +29,19 @@ public class RestApiApplication {
         SpringApplication.run(RestApiApplication.class, args);
     }
 
+    // INTERNATIONALIZATION
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+//    THIS APPROACH APPLIES ONLY FOR INTERNATIONALIZATION OPTION 1
+//    @Bean
+//    public ResourceBundleMessageSource messageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasename("messages");
+//        return messageSource;
+//    }
 }
